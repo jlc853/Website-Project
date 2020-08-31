37c69c02e6bd8145d1ed46e39314af6cc45a88ad
jlc853 <jlc853@cs.bham.ac.uk>
2020-08-31 16:35:16 +0100

    Added post editing, Removed Secret Key and Debuging

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), # pylint: disable=maybe-no-member
+    path('post/new/', views.post_new, name='post_new'), # pylint: disable=maybe-no-member
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), # pylint: disable=maybe-no-member
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&9j=o%d+h)tq633j7nc-)f&0wup5s@(=0fzf)vc%z^l3=%qir+'
+with open(BASE_DIR+'\secret_key') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
