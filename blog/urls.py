75ac4c639c0e1918d7f603aba3ad6e12ffbfb13c
jlc853 <jlc853@cs.bham.ac.uk>
2020-08-31 23:17:52 +0100

    Added CV page

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'), # pylint: disable=maybe-no-member
     path('post/new/', views.post_new, name='post_new'), # pylint: disable=maybe-no-member
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), # pylint: disable=maybe-no-member
+    path('CV/', views.CV, name='CV'), # pylint: disable=maybe-no-member
 ]
--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def CV(request):
+    return render(request, 'CV/CV.html')
