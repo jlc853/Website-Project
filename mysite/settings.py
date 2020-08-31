e5da600d978333c310334e36662866c6c026f67b
jlc853 <jlc853@cs.bham.ac.uk>
2020-08-31 17:44:41 +0100

    Implemented SECRET_KEY in pythonanywhere as a environment variable

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = ""
 if platform == "linux" or platform == "linux2":
     SECRET_KEY = os.environ['SECRET_KEY']
 elif platform == "win32":
